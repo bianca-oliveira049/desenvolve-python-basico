77ccf7a5be19f0e0eb038ca65dba1bd55b1540f2
bianca-oliveira049 <163353739+bianca-oliveira049@users.noreply.github.com>
2024-05-02 10:32:01 -0300

    2.2 - Tipos de dados

---
+++ modulo2/aula2_questao1.py
@@ -0,0 +1,7 @@
+num1 = 5
+num2 = 2
+print(type(num1))
+print(type(num2))
+divisao = num1 / num2
+print(divisao)
+print(type(divisao))
---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,12 @@
+texto = "o resultado é"
+num1 = 10
+num2 = 3.5
+soma = num1 + num2
+print(texto)
+print(num1)
+print(num2)
+print(soma)
+print(type(texto))
+print(type(num1))
+print(type(num2))
+print(type(soma))
